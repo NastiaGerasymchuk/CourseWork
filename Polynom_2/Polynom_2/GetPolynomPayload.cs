0f8b2c566ac05c88ced100ed4cde85717fef1c23
Nastia <gerasymchuk88888888@gmail.com>
2022-03-20 22:17:00 +0100

    add desctop work

---
+++ Polynom_2/Polynom_2/GetPolynomPayload.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+   public class GetPolynomPayload
+    {
+          public UInt64 start { get; set; }
+          public int countQuestionsFunction { get; set; }
+          public int k { get; set; }
+          public int n { get; set; }
+          public int rowCount { get; set; }
+          public int[][] arrV { get; set; }
+          public string[] pArr { get; set; }
+         public string fX { get; set; }
+    }
+}
---
+++ Polynom_2/Polynom_2/HelpForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class HelpForm : Form
+    {
+        public HelpForm()
+        {
+            InitializeComponent();
+        }
+
+        private void HelpForm_Load(object sender, EventArgs e)
+        {
+            txtBoxfaculty.SelectionStart = txtBoxfaculty.Text.Length;
+            txtBoxCurseWork.SelectionStart = txtBoxCurseWork.Text.Length;
+            txtBoxPolynom.SelectionStart = txtBoxPolynom.Text.Length;
+            txtBoxMyName.SelectionStart = txtBoxMyName.Text.Length;
+            txtBoxTeacherName.SelectionStart = txtBoxTeacherName.Text.Length;
+            txtBoxCity.SelectionStart = txtBoxCity.Text.Length;
+            
+            //Відмінити автовибір для текстових полів//
+            txtBoxfaculty.DeselectAll();
+            txtBoxCurseWork.DeselectAll();
+            txtBoxPolynom.DeselectAll();
+            txtBoxMyName.DeselectAll();
+            txtBoxTeacherName.DeselectAll();
+            txtBoxCity.DeselectAll();
+        }       
+    }
+}
